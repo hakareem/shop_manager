bf433052bcba506183a13c59a2e7a7e4b203ce86
hakareem <harithakareem72@gmail.com>
2022-08-25 20:52:15 +0100

    database with ids association

--- spec/seeds.sql
+++ spec/seeds.sql
@@ -1,10 +1,16 @@
-TRUNCATE TABLE items, orders RESTART IDENTITY;
+TRUNCATE TABLE items, orders, items_orders RESTART IDENTITY;
 
 INSERT INTO items (name, unit_price, quantity) VALUES ('iPhone', 20, 5);
 INSERT INTO items (name, unit_price, quantity) VALUES ('Tv', 50, 10);
 INSERT INTO items (name, unit_price, quantity) VALUES ('Apple', 10, 8);
 
 
-INSERT INTO orders (customer_name, date, item_id) VALUES ('Penaldo', '2022-03-01', 1);
-INSERT INTO orders (customer_name, date, item_id) VALUES ('Penzema', '2022-12-04', 2);
-INSERT INTO orders (customer_name, date, item_id) VALUES ('Messi', '2022-10-06', 3);
+INSERT INTO orders (customer_name, date) VALUES ('Penaldo', '2022-03-01');
+INSERT INTO orders (customer_name, date) VALUES ('Penzema', '2022-12-04');
+INSERT INTO orders (customer_name, date) VALUES ('Messi', '2022-10-06');
+
+
+INSERT INTO items_orders (item_id, order_id) VALUES
+(1,1),
+(2,2),
+(3,2),(3,3);
