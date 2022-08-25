3d7688d6ae3e45f8177a8d2c6b6a64c63a0c6d83
hakareem <harithakareem72@gmail.com>
2022-08-25 11:54:34 +0100

    truncating data from db

--- spec/seeds.sql
+++ spec/seeds.sql
@@ -1,7 +1,10 @@
 TRUNCATE TABLE items, orders RESTART IDENTITY;
 
-INSERT INTO items (name, price, quantity) VALUES ('iPhone', 10, 2);
-INSERT INTO items (name, price, quantity) VALUES ('TV', 30, 4);
+INSERT INTO items (name, unit_price, quantity) VALUES ('iPhone', 20, 5);
+INSERT INTO items (name, unit_price, quantity) VALUES ('Tv', 50, 10);
+INSERT INTO items (name, unit_price, quantity) VALUES ('Apple', 10, 8);
 
-INSERT INTO orders (customer_name, order_date, item_id) VALUES ('Penaldo', '2022-12-13', 1 );
-INSERT INTO orders (customer_name, order_date, item_id) VALUES ('Penzema', '2022-08-08', 2);
+
+INSERT INTO orders (customer_name, date, item_id) VALUES ('Penaldo', '2022-03-01', 1);
+INSERT INTO orders (customer_name, date, item_id) VALUES ('Penzema', '2022-12-04', 2);
+INSERT INTO orders (customer_name, date, item_id) VALUES ('Messi', '2022-10-06', 3);
