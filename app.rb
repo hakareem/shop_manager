534778c9ff8c45f527eb8535b86679a698228791
hakareem <harithakareem72@gmail.com>
2022-08-25 14:06:07 +0100

    application class interface

---
+++ app.rb
@@ -0,0 +1,25 @@
+require_relative './lib/item_repository'
+require_relative './lib/order_repository'
+
+class Application
+
+  def initialize(database_name, io, album_repository, artist_repository)
+    DatabaseConnection.connect(database_name)
+    @io = io
+    @album_repository = album_repository
+    @artist_repository = artist_repository
+  end
+
+  def run
+  end
+end
+
+if __FILE__ == $0
+  app = Application.new(
+    'shop_manager_test',
+    Kernel,
+    ItemRepository.new,
+    OrderRepository.new
+  )
+  app.run
+end
